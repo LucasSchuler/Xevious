ad99bdae640b585c293d89d3fbcac759950d0053
unknown <Antonio_Carneiro@8116KH0Y1.poa.amer.dell.com>
2015-05-11 17:32:09 -0300

    Limpeza de arquivos não utilizados, mudança do nome do projeto e dos diretórios.... compilado e funcionando.

---
+++ examples/Xevious_Remake/main.cpp
@@ -0,0 +1,34 @@
+/*
+ *  XEVIOUS REMAKE
+ *  ANTONIO KINDERMANN E LUCAS SCHULER
+ *
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+#include "Game.h"
+#include "PlayState.h"
+
+int main(int argc, char **argv)
+{
+    cgf::Game game(5,30);
+
+	game.init("Framework com estados, sprites e tiling",320,640,false);
+
+    game.changeState(PlayState::instance());
+
+	while(game.isRunning())
+	{
+		game.handleEvents();
+		game.update();
+		game.draw();
+	}
+
+	// cleanup the engine
+	game.clean();
+
+    return 0;
+}
